548f12b11c7911a2fc681af1eeccf8e84bceabc5
SenItaNa <smalljie233@gmail.com>
2026-06-16 16:38:33 +0800

    tune: fix forward balance direction

--- HAREWER/BALANCE/balance.h
+++ HAREWER/BALANCE/balance.h
@@ -6,7 +6,7 @@
 #define BALANCE_LOOP_MS        5
 #define BALANCE_PWM_LIMIT      2500
 #define BALANCE_SAFE_ANGLE_X100 1500
-#define BALANCE_OUTPUT_SIGN    -1
+#define BALANCE_OUTPUT_SIGN    1
 
 extern float Balance_Angle_Kp;
 extern float Balance_Angle_Kd;
