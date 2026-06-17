92baeef884f76b876fbebf8ad51fc73b443288f5
SenItaNa <smalljie233@gmail.com>
2026-06-17 18:08:42 +0800

    refactor: move balance loop into timer interrupt

--- USER/main.c
+++ USER/main.c
@@ -23,6 +23,14 @@ int TargetVelocity = 0;
 u16 angle_adc;
 int zero_offset = 2048;
 int angle_x100;
+int encoder[4] = {0, 0, 0, 0};
+int pwm[4] = {0, 0, 0, 0};
+int balance_pwm = 0;
+u16 battery_adc;
+float vcc = 0;
+volatile u8 lcd_update_flag = 0;
+volatile u8 serial_update_flag = 0;
+volatile u8 key_update_flag = 0;
 
 static void Startup_Mark(u8 line, char *text)
 {
@@ -30,11 +38,46 @@ static void Startup_Mark(u8 line, char *text)
 	LCD_ShowString(0, line, (u8*)text, GREEN, BLACK, 16, 0);
 }
 
+static void Balance_Timer_Set(u8 enable)
+{
+	if(enable) TIM_Cmd(TIM1, ENABLE);
+	else TIM_Cmd(TIM1, DISABLE);
+}
+
+static void Balance_Timer_Init(void)
+{
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+	TIM_TimeBaseStructure.TIM_Prescaler = 7200 - 1;  // 72MHz / 7200 = 10kHz
+	TIM_TimeBaseStructure.TIM_Period = BALANCE_LOOP_MS * 10 - 1;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+	TIM_ClearFlag(TIM1, TIM_FLAG_Update);
+	TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
+
+	NVIC_InitStructure.NVIC_IRQChannel = TIM1_UP_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	TIM_Cmd(TIM1, ENABLE);
+}
+
 static void Calibrate_Angle_Zero(void)
 {
+	Balance_Timer_Set(0);
+	Balance_Stop();
+	Set_PWM(0, 0, 0, 0);
 	zero_offset = Get_adc_Average(ADC_Channel_14, 50);
 	angle_adc = zero_offset;
 	angle_x100 = 0;
+	Balance_Timer_Set(1);
 }
 
 static void LCD_Show_Debug(int *enc, int *pwm, float vcc)
@@ -124,15 +167,6 @@ static void Handle_Serial_Command(void)
 
 int main(void)
 {
-	int encoder[4] = {0, 0, 0, 0};
-	int pwm[4] = {0, 0, 0, 0};
-	u16 adcx;
-	float vcc = 0;
-	u16 lcd_tick = 0;
-	u16 serial_tick = 0;
-	u16 vbat_tick = 0;
-	int balance_pwm;
-
 	SystemInit();
 	delay_init();
 	uart_init(115200);
@@ -161,16 +195,51 @@ int main(void)
 	Encoder_Timer_Init();
 	Startup_Mark(80, "ENC TIMER OK");
 #endif
+	Balance_Timer_Init();
 
 	printf("Cart balance ready. Hold pendulum vertical, press KEY2 to zero and start.\r\n");
 
 	while(1)
 	{
+		if(key_update_flag)
+		{
+			key_update_flag = 0;
+			Handle_Key(KEY_Scan());
+		}
+		Handle_Serial_Command();
+
+		if(serial_update_flag)
+		{
+			serial_update_flag = 0;
+			printf("BAL=%d Stop=%d V=%.2f A=%d B=%d C=%d D=%d PWM=%d Ang=%d.%02d Rate=%d SF=%d ADC=%d Off=%d\r\n",
+				Balance_Enable, Balance_Stop_Reason, vcc,
+				encoder[0], encoder[1], encoder[2], encoder[3],
+				balance_pwm,
+				angle_x100 / 100, (angle_x100 < 0 ? -angle_x100 : angle_x100) % 100,
+				Balance_Angle_Rate_X100, Balance_Speed_Filter, angle_adc, zero_offset);
+		}
+
+		if(lcd_update_flag)
+		{
+			lcd_update_flag = 0;
+			LCD_Show_Debug(encoder, pwm, vcc);
+		}
+	}
+}
+
+void TIM1_UP_IRQHandler(void)
+{
+	static u8 serial_tick = 0;
+	static u8 lcd_tick = 0;
+	static u8 vbat_tick = 0;
+
+	if(TIM_GetITStatus(TIM1, TIM_IT_Update) != RESET)
+	{
+		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+		key_update_flag = 1;
+
 		angle_adc = Get_adc(ADC_Channel_14);
 		angle_x100 = ((int)angle_adc - zero_offset) * 34000 / 4096;
-
-		Handle_Key(KEY_Scan());
-		Handle_Serial_Command();
 
 		encoder[0] = Read_Encoder(8);
 		encoder[1] = Read_Encoder(2);
@@ -187,27 +256,20 @@ int main(void)
 		if(++vbat_tick >= 50)
 		{
 			vbat_tick = 0;
-			adcx = Get_adc(ADC_Channel_5);
-			vcc = (float)adcx * (3.3 * 11 / 4096);
+			battery_adc = Get_adc(ADC_Channel_5);
+			vcc = (float)battery_adc * (3.3 * 11 / 4096);
 		}
 
 		if(++serial_tick >= 10)
 		{
 			serial_tick = 0;
-			printf("BAL=%d Stop=%d V=%.2f A=%d B=%d C=%d D=%d PWM=%d Ang=%d.%02d Rate=%d SF=%d ADC=%d Off=%d\r\n",
-				Balance_Enable, Balance_Stop_Reason, vcc,
-				encoder[0], encoder[1], encoder[2], encoder[3],
-				balance_pwm,
-				angle_x100 / 100, (angle_x100 < 0 ? -angle_x100 : angle_x100) % 100,
-				Balance_Angle_Rate_X100, Balance_Speed_Filter, angle_adc, zero_offset);
+			serial_update_flag = 1;
 		}
 
 		if(++lcd_tick >= 50)
 		{
 			lcd_tick = 0;
-			LCD_Show_Debug(encoder, pwm, vcc);
+			lcd_update_flag = 1;
 		}
-
-		delay_ms(BALANCE_LOOP_MS);
 	}
 }
