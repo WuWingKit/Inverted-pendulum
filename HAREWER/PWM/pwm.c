ec03a7f6ea3d60c6d99569da2184c67ac4d0a561
SenItaNa <smalljie233@gmail.com>
2026-06-17 18:35:35 +0800

    fix: add motor pwm deadzone compensation

--- HAREWER/PWM/pwm.c
+++ HAREWER/PWM/pwm.c
@@ -2,6 +2,9 @@
 #include "math.h"
 #include <stdlib.h>
 
+#define MOTOR_PWM_MAX       7199
+#define MOTOR_PWM_DEADZONE  1100
+
 /**************************************************************************
 函数功能：pwm初始化
 入口参数：arr：设为一个时钟频率的最大值  psc： 预分频值
@@ -49,8 +52,26 @@ int myabs(int value){
 	if (value<0) value=-value;
 	return value;
 }
+static int Apply_PWM_Deadzone(int value)
+{
+	int pwm;
+	int sign;
+
+	if(value == 0) return 0;
+
+	sign = value > 0 ? 1 : -1;
+	pwm = myabs(value) + MOTOR_PWM_DEADZONE;
+	if(pwm > MOTOR_PWM_MAX) pwm = MOTOR_PWM_MAX;
+
+	return sign * pwm;
+}
 void Set_PWM(int motor1,int motor2,int motor3,int motor4)
 {
+	 motor1 = Apply_PWM_Deadzone(motor1);
+	 motor2 = Apply_PWM_Deadzone(motor2);
+	 motor3 = Apply_PWM_Deadzone(motor3);
+	 motor4 = Apply_PWM_Deadzone(motor4);
+
 	if(motor1>0){
 		GPIO_SetBits(GPIOC, GPIO_Pin_14);	 // 高电平      PC14 --- AIN2      1   
 		GPIO_ResetBits(GPIOC, GPIO_Pin_13);	 // 低电平}   PC13 --- AIN1      0
